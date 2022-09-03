72cb874b9ac65e68be8bf6b5075561e5675b4ae6
Hari0024 <112777989+Hari0024@users.noreply.github.com>
2022-09-04 00:28:26 +0530

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class Fibonacci {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int firstnum = 0;
+		int secnum = 1;
+		int sum = 0;
+		System.out.println(firstnum);
+		for (int i=1;i<=10;i++) {
+			sum=firstnum+secnum;
+			firstnum=secnum;
+			secnum=sum;
+			System.out.println(sum);
+		}
+
+	}
+
+}
