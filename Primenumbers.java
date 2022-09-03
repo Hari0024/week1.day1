533f4a1bfc1069d20067fb668feeba4acd3d2ac7
Hari0024 <112777989+Hari0024@users.noreply.github.com>
2022-09-04 00:58:06 +0530

    Add files via upload

---
+++ Primenumbers.java
@@ -0,0 +1,25 @@
+package week1.day1;
+
+public class Primenumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num =35;
+		boolean flag =false;
+		for(int i=2;i<=num/2;++i) {
+			//condition for non-prime number
+			if (num % i==0) {
+				flag =true;
+		break;
+		
+		 
+
+	}
+
+}
+if (!flag)
+System.out.println(num+ "  " +"is a prime Number");
+else
+	System.out.println(num+ "  " +"is not a prime number");
+}
+}
